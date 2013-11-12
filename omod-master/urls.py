ada4307cce82ae10533805883b339797c61f9a8d
wusz <and80506@163.com>
2013-11-12 23:46:47 +0800

    first check in
    Signed-off-by: wusz <and80506@163.com>

--- omod-master/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls.defaults import patterns, include, url
-from django.conf import settings
-
-urlpatterns = patterns('',
-    url(r'^omod/', include("mods.urls")),
-     #static files
-    url(r'^omod/media/(?P<path>.*)$','django.views.static.serve',{'document_root':settings.STATIC_ROOT}),
-)
